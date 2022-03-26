380f3b16c70963870cafb00fb640be29c7f46463
Francisco Cuevas <fmcuevas1985@outlook.com>
2022-03-26 20:05:49 -0300

    methods

--- proyectos/basics-01-starting-code/app.js
+++ proyectos/basics-01-starting-code/app.js
@@ -4,20 +4,23 @@ const app = Vue.createApp({
 			courseGoal: 'Finish the course and learn Vue!',
 			vueLink: 'https://vuejs.org/'
 		};
+	},
+	methods: {
+		outputGoal() {
+			const randomNumber = Math.random();
+			if (randomNumber < 0.5) {
+				return 'Learn Vue!';
+			} else {
+				return 'Master Vue!';
+			}
+		}
 	}
 });
 
 app.mount('#user-goal');
 
+
 /*
-generar datos con interpolacion
-
-generar datos
-la sintaxis {{}} solo esta disponible para etiquetas html
-Para pasar un valor dinamico a un atributo en HTML usamos la sintaxis de enlace de Vue, directiva Vue
-
-v-bind : es un nombre reservado, detectado y entendido por Vue.js, una directiva es basicamente
-una instruccion, una instruccion que le damos a Vue.js para que haga algo, v-bind le dice a Vue.js
-que vincule, para que establezca el valor de algo. Que puede ser un atributo en un elemento HTML.
-
+metodos:
+permiten definir funciones que deben ejecutarse cuando se las llama.
  */
