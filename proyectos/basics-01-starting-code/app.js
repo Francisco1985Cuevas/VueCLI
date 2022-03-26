249a14d7eba084bddb73406f4184fc43e82e019a
Francisco Cuevas <fmcuevas1985@outlook.com>
2022-03-26 17:47:08 -0300

    v-bind

--- proyectos/basics-01-starting-code/app.js
+++ proyectos/basics-01-starting-code/app.js
@@ -1,9 +1,23 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			courseGoal: 'Finish the course and learn Vue!'
+			courseGoal: 'Finish the course and learn Vue!',
+			vueLink: 'https://vuejs.org/'
 		};
 	}
 });
 
 app.mount('#user-goal');
+
+/*
+generar datos con interpolacion
+
+generar datos
+la sintaxis {{}} solo esta disponible para etiquetas html
+Para pasar un valor dinamico a un atributo en HTML usamos la sintaxis de enlace de Vue, directiva Vue
+
+v-bind : es un nombre reservado, detectado y entendido por Vue.js, una directiva es basicamente
+una instruccion, una instruccion que le damos a Vue.js para que haga algo, v-bind le dice a Vue.js
+que vincule, para que establezca el valor de algo. Que puede ser un atributo en un elemento HTML.
+
+ */
