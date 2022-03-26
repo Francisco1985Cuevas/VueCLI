4d799f356ce5da426a65351f1663de09afa7b0c7
Francisco Cuevas <fmcuevas1985@outlook.com>
2022-03-26 16:36:55 -0300

    Video 15, donde se muestra la interpolacion propiedades en Vue

--- proyectos/gs-01-starting-project/app.js
+++ proyectos/gs-01-starting-project/app.js
@@ -1,19 +1,31 @@
 /*
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+//Para resolver este problema, usamos lo que en JavasCript 
+//se llama el enfoque imperativo donde definimos paso a paso 
+//que debe ejecutar el navegador...
 
+const buttonEl = document.querySelector('button'); //obtenga el acceso al boton
+const inputEl = document.querySelector('input'); //obtenga el acceso al elemento de entrada
+const listEl = document.querySelector('ul'); //obtenga el acceso al elemento de la lista
+
+//crea una funcion
 function addGoal () {
-	const enteredValue = inputEl.value;
-	const listItemEl = document.createElement('li');
-	listItemEl.textContent = enteredValue;
-	listEl.appendChild(listItemEl);
+	const enteredValue = inputEl.value;	//obtenga acceso al valor ingresado
+	const listItemEl = document.createElement('li'); //cree un elemento de element List
+	listItemEl.textContent = enteredValue; //agrega el contenido del texto
+	listEl.appendChild(listItemEl); //agregue este elemento de lista al valor.
 	inputEl.value = '';
 }
 
+//agregue un controlador de eventos de click al boton y ejecute esta funcion si hago click...
 buttonEl.addEventListener('click', addGoal); //la funcion se ejecuta cuando ocurre un click
 */
 
+
+
+
+//Vue.js presenta otro enfoque o otra manera de resolver este mismo problema..
+//Definimos el resultado final deseado y los datos que necesitaremos en nuestra aplicacion Vue.js
+//y lo dejaremos para ser visualizado.
 Vue.createApp({
 	data() {
 		return {
